170bc5b8e0925a3a21138c73edd7419e24e40f53
zhang <534780792@qq.com>
2016-08-10 16:46:39 +0800

    ListView

---
+++ app/src/main/java/com/zhangpeng/administrator/listviewtest/MainActivity.java
@@ -0,0 +1,29 @@
+package com.zhangpeng.administrator.listviewtest;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import static android.R.id.list;
+
+public class MainActivity extends AppCompatActivity {
+    ListView list;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        list=(ListView)findViewById(R.id.listview);
+        MyAdapter adapter=new MyAdapter(this);
+        list.setAdapter(adapter);
+    }
+
+
+}
